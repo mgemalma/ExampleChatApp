4ea40a6b8d60955b075ebd5769d7cdba2a3fd2bf
altuggemalmaz <altuggemalmaz1@gmail.com>
2018-05-18 19:48:10 -0400

    before the disaster

--- app/src/main/java/com/example/altuggemalmaz/lapitchat/ChatActivity.java
+++ app/src/main/java/com/example/altuggemalmaz/lapitchat/ChatActivity.java
@@ -211,7 +211,7 @@ public class ChatActivity extends AppCompatActivity {
 
     private void loadMessages()
     {
-        
+
         //The reference to the DB where the messages are stored
         //We are going to use Child Event Listener to deal with multiple things and load all the messages
         mRootRef.child("messages").child(mCurrentUser).child(mChatUser).addChildEventListener(new ChildEventListener() {
